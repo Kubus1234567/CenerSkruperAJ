480fdc4c7dd8ff174dd03dd200eea6f45bd8b658
Kubus1234567 <s218302@student.uek.krakow.pl>
2021-04-29 16:53:12 +0200

    2021-04-29 CP2

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+# import Flask class from flask package
+from flask import Flask
+
+# definition of Flask object called app
+app = Flask(__name__)
+
+#import of routings (route module) from app package
+from app import routes
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0',port=8081)
